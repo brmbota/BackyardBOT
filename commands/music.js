521275b7de68b0149dcfd0a20d43e6e2081cc7ee
brmbota <brmbota14@gmail.com>
2021-09-17 16:27:26 +0200

    Changed music command for testing

--- commands/music.js
+++ commands/music.js
@@ -32,19 +32,20 @@ module.exports = {
             if (ytdl.validateURL(args[2])) {
                 const song_info = await ytdl.getInfo(args[2]);
                 song = { title: song_info.videoDetails.title, url: song_info.videoDetails.video_url }
-            } else {
-                //If there was no link, we use keywords to search for a video. Set the song object to have two keys. Title and URl.
-                const video_finder = async (query) => {
-                    const video_result = await ytSearch(query);
-                    return (video_result.videos.length > 1) ? video_result.videos[0] : null;
-                }
-                const video = await video_finder(args.join(' ').replace("music play", ""));
-                if (video) {
-                    song = { title: video.title, url: video.url }
-                } else {
-                    message.channel.send('Error finding video.');
-                }
             }
+            // } else {
+            //     //If there was no link, we use keywords to search for a video. Set the song object to have two keys. Title and URl.
+            //     const video_finder = async (query) => {
+            //         const video_result = await ytSearch(query);
+            //         return (video_result.videos.length > 1) ? video_result.videos[0] : null;
+            //     }
+            //     const video = await video_finder(args.join(' ').replace("music play", ""));
+            //     if (video) {
+            //         song = { title: video.title, url: video.url }
+            //     } else {
+            //         message.channel.send('Error finding video.');
+            //     }
+            // }
             //If the server queue does not exist (which doesn't for the first video queued) then create a constructor to be added to our global queue.
             if (!server_queue) {
 
